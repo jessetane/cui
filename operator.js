ca676dd18def904bcebb568bc71f1d21475bc5d5
Jesse Tane <jesse.tane@gmail.com>
2012-10-12 21:45:09 -0400

    ui functions moved to own module

--- operator.js
+++ operator.js
@@ -6,20 +6,21 @@
  */
 
 
+require("coffee-script");  // support operations written in CS
+
 var fs = require("fs");
 var path = require("path");
 var crypto = require("crypto");
 var _ = require("underscore");
+var ui = require("./ui");
 var args = process.argv.slice(2);
-var sequence = [];
-var operator = { results: [] };
-require("coffee-script");
+var operator = { results: [], sequence: [] };
 
 
 module.exports = load;
 
 if (require.main == module) {
-  findOperations(process.cwd(), function (err, dir, operations) {
+  find(process.cwd(), function (err, dir, operations) {
     if (!err) {
       load(operations, dir);
     } else {
@@ -28,41 +29,40 @@ if (require.main == module) {
   });
 }
 
-
-function load (operations, cacheDir) {
-  if (!cacheDir) cacheDir = process.cwd();
-  fs.readFile(".operator", "utf8", function (err, data) {
-    if (!err) operator.cache = JSON.parse(data);
-    processOperations(operations(operator));
-  });
-}
-
-function findOperations (dir, cb) {
+function find (dir, cb) {
   try {
     var filename = path.normalize(dir + "/operations");
     var operations = require(filename);
     cb(null, dir, operations);
   } catch (err) {
     if (dir !== "/" && err.code === "MODULE_NOT_FOUND") {
-      findOperations(path.normalize(dir + "/.."), cb);
+      find(path.normalize(dir + "/.."), cb);
     } else {
       cb(err);
     }
   }
 }
 
-function processOperations (operations) {
+function load (operations, cacheDir) {
+  if (!cacheDir) cacheDir = process.cwd();
+  fs.readFile(".operator", "utf8", function (err, data) {
+    if (!err) operator.cache = JSON.parse(data);
+    run(operations(operator));
+  });
+}
+
+function run (operations) {
   switch (operations.type) {
     case "sequence":
       if (operations.prompt) console.log(operations.prompt);
-      sequence = operations.data.concat(sequence);
-      sequencer();
+      operator.sequence = operations.data.concat(operator.sequence);
+      next();
       break;
     case "executable":
       var cb = function (err, result) {
         if (err) return done(err);
         operator.results.push(result);
-        sequencer();
+        next();
       };
       var result = operations.data(cb);
       if (result) cb(null, result);
@@ -70,28 +70,42 @@ function processOperations (operations) {
     case "question":
       getDynamicData(operations.data, function (err, questions) {
         if (err) return done(err);
-        if (operations.prompt) console.log(operations.prompt);
-        askQuestions(questions, function (answers) {
+        ui.prompt(operations);
+        ui.askQuestions(questions, getAnswersFor(questions), function (answers) {
           operator.results = operator.results.concat(answers);
-          sequencer();
+          next();
         });
       });
       break;
     case "choice":
       getDynamicData(operations.data, function (err, options) {
         if (err) return done(err);
-        if (operations.prompt) console.log("• " + operations.prompt);
-        else console.log("• ");
-        listOptions(options, operations.properties);
-        chooseOption(options, operations.properties, function (choice) {
+        ui.prompt(operations);
+        ui.listOptions(options, operations.properties);
+        ui.chooseOption(options, operations.properties, getChoiceFor(options), function (choice) {
           operator.results.push(choice);
-          sequencer();
+          next();
         });
       });
       break;
     default:
       done(new Error("Unrecognized operation type \"" + operations.type + "\""));
       break;
+  }
+}
+
+function next () {
+  if (operator.sequence.length) {
+    run(operator.sequence.shift());
+  } else if (operator.results.length) {
+    var result = operator.results.slice(-1)[0];
+    if (result && result.type && result.data) {
+      run(result);
+    } else {
+      done();
+    }
+  } else {
+    done();
   }
 }
 
@@ -114,19 +128,21 @@ function getDynamicData (f, cb) {
   }
 }
 
-function sequencer () {
-  if (sequence.length) {
-    processOperations(sequence.shift());
-  } else if (operator.results.length) {
-    var result = operator.results.slice(-1)[0];
-    if (result && result.type && result.data) {
-      processOperations(result);
-    } else {
-      done();
-    }
-  } else {
-    done();
+function getAnswersFor (questions) {
+  var answers = null;
+  if (args.length >= questions.length) {
+    answers = args.slice(0, questions.length);
+    args = args.slice(questions.length);
   }
+  return answers;
+}
+
+function getChoiceFor (options) {
+  var choice = null;
+  if (args.length >= 1) {
+    choice = args.shift();
+  }
+  return choice;
 }
 
 function done (err) {
@@ -137,89 +153,4 @@ function done (err) {
       if (err) console.log("Failed to save cache");
     });
   }
-}
-
-function listOptions (options, props) {
-  options.forEach(function (option, i) {
-    var bracket = (i === options.length-1) ? "└─" : "├─";
-    console.log(bracket + "[ " + (i+1) + " ]─ " + makeOptionLabel(option, props));
-  });
-}
-
-function chooseOption (options, props, cb) {
-  var rl = readline()
-  rl.question("Enter a number [1-" + options.length + "]: ", function (i) {
-    rl.close();
-    var choice = selectNumericalOption(options, i);
-    if (choice) {
-      var label = makeOptionLabel(choice, props);
-      console.log("You chose \"" + label + "\"");
-      cb(choice);
-    } else {
-      console.log("Invalid choice");
-      chooseOption(options, props, cb);
-    }
-  });
-}
-
-function makeOptionLabel(option, props) {
-  var label = option;
-  if (typeof option !== "string") {
-    if (props) {
-      if (typeof props === "string") {
-        label = option[props];
-      } else {
-        label = props.map(function (prop) { return option[prop] });
-        label = label.join(" ─ ");
-      }
-    } else {
-      label = getDynamicData(option.name);
-    }
-  }
-  return label;
-}
-
-function selectNumericalOption (options, choice) {
-  var choice = Number(choice);
-  if (choice === NaN || choice > options.length || choice < 1) {
-    return null;
-  } else {
-    return options[Math.floor(choice)-1];
-  }
-}
-
-function askQuestions (questions, cb) {
-  if (!questions.forEach) { // if the questions are in a hash, wrap the cb to deal with it
-    var keys = Object.keys(questions);
-    var wrapped = cb;
-    cb = function (answers) {
-      var hash = {};
-      keys.forEach(function (key, i) { if (answers[i]) hash[key] = answers[i] });
-      wrapped(hash);
-    }
-    questions = _.values(questions);
-  }
-  var answers = [];
-  if (typeof questions == "string") questions = [ questions ];
-  ask = function () {
-    if (!questions || questions.length === 0) {
-      cb(answers);
-    } else {
-      var rl = readline();
-      rl.question("• " + questions.shift(), function (answer) {
-        answers.push(answer || undefined);
-        rl.close();
-        ask();
-      });
-    }
-  }
-  ask();
-}
-
-function readline () {
-  var readline = require("readline");
-  return readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
 }
---
+++ ui.js
@@ -0,0 +1,124 @@
+/*
+ *  ui.js
+ *
+ */
+
+
+// exports:
+// - prompt
+// - listOptions
+// - chooseOption
+// - askQuestions
+
+
+exports.prompt = function prompt (operations) {
+  switch (operations.type) {
+    case "question":
+      if (operations.prompt) console.log(operations.prompt);
+      break;
+    case "choice":
+      if (operations.prompt) console.log("• " + operations.prompt);
+      else console.log("• ");
+  }
+}
+
+exports.listOptions = function listOptions (options, props, auto) {
+  options.forEach(function (option, i) {
+    var bracket = (i === options.length-1) ? "└─" : "├─";
+    console.log(bracket + "[ " + (i+1) + " ]─ " + makeOptionLabel(option, props));
+  });
+}
+
+exports.chooseOption = function chooseOption (options, props, preChoosen, cb) {
+  var rl = readline(preChoosen || null);
+  rl.question("Enter a number [1-" + options.length + "]: ", function (i) {
+    rl.close();
+    var choice = selectNumericalOption(options, props, i);
+    if (choice) {
+      var label = makeOptionLabel(choice, props);
+      console.log("You chose \"" + label + "\"");
+      cb(choice);
+    } else {
+      console.log("Invalid choice");
+      chooseOption(options, props, null, cb);
+    }
+  });
+}
+
+exports.askQuestions = function askQuestions (questions, preAnswered, cb) {
+  if (!questions.forEach) { // if the questions are in a hash, wrap the cb to deal with it
+    var keys = Object.keys(questions);
+    var wrapped = cb;
+    cb = function (answers) {
+      var hash = {};
+      keys.forEach(function (key, i) { if (answers[i]) hash[key] = answers[i] });
+      wrapped(hash);
+    }
+    questions = _.values(questions);
+  }
+  var answers = [];
+  if (typeof questions == "string") questions = [ questions ];
+  ask = function () {
+    if (!questions || questions.length === 0) {
+      cb(answers);
+    } else {
+      var rl = readline(preAnswered && preAnswered.shift() || null);
+      rl.question("• " + questions.shift(), function (answer) {
+        answers.push(answer || undefined);
+        rl.close();
+        ask();
+      });
+    }
+  }
+  ask();
+}
+
+function makeOptionLabel(option, props) {
+  var label = option;
+  if (typeof option !== "string") {
+    props = props || "name";
+    if (typeof props === "string") {
+      label = option[props];
+    } else {
+      label = props.map(function (prop) { return option[prop] });
+      label = label.join(" ─ ");
+    }
+  }
+  return label;
+}
+
+function selectNumericalOption (options, props, choice) {
+  var n = Number(choice);
+  if (isNaN(n)) {
+    var match = null;
+    options.forEach(function (option, i) {
+      var label = makeOptionLabel(option, props).toLowerCase();
+      if (label.search(choice.toLowerCase()) > -1) {
+        match = options[i];
+      }
+    });
+    return match;
+  } else if (n > options.length || n < 1) {
+    return null;
+  } else {
+    return options[Math.floor(n)-1];
+  }
+}
+
+function readline (result) {
+  if (result) {
+    return {
+      question: function (query, cb) {
+        console.log(query + result);
+        cb(result);
+      },
+      close: function () {}
+    }
+  } else {
+    var readline = require("readline");
+    return readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
+}
